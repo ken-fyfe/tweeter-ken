a1d110f427461efc3c10cb7489d96dcf9759c0bc
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-28 15:33:37 +0000

    w4 d1 assignments bacially working

--- server/index.js
+++ server/index.js
@@ -30,5 +30,6 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  const dateTime = new Date().toLocaleString('en-US', {timeZone: 'America/Edmonton'});
+  console.log(`Example app listening on port ${PORT}. Started at ${dateTime}.`);
 });
